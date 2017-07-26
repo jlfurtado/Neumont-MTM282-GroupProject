f135fe7ba9b237ac9fa8c252b88e8cfa1e2608b4
ksHDO <hiddendeathorb@gmail.com>
2017-07-26 16:03:39 -0600

    - basic css - directions.pug

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/', function(req, res) {
     res.render('index', {
-        title: 'Website',
+        title: 'website',
         config: config
     });
 });
