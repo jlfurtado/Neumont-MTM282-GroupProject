faacef8c5bc4f534b67915ea5f0f6cb942803a35
ksHDO <hiddendeathorb@gmail.com>
2017-07-26 08:51:37 -0600

    index.js and config.json

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    pug = require('pug'),
+    path = require('path'),
+    config = require('./config');
+
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.get('/', function(req, res) {
+    res.render('index', {
+        title: 'Website',
+        config: config
+    });
+});
+
+app.get('/:page', function(req, res) {
+    res.render(req.params.page, {
+        title: req.params.page,
+        config: config
+    });
+});
+
+app.listen(3000);
